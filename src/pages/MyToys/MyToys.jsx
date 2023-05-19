081bac81aa72bc7e146981d2e6cab86a9063884f
mfarhadattari <mfarhad.attari@gmail.com>
2023-05-19 12:12:29 +0600

    remove a toy functionality added

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -11,10 +11,10 @@ const MyToys = () => {
   const [loading, setLoading] = useState(true);
 
   /* ------------------------------------------------------------------
-        ! --------------------- load my toys ----------------------
+        ! --------------------- | LOAD MY TOYS | ----------------------
     --------------------------------------------------------------------- */
   useEffect(() => {
-    fetch(`https://mfarhad-toy-master.vercel.app/my-toys?email=${user.email}`, {
+    fetch(`http://localhost:5000/my-toys?email=${user.email}`, {
       method: "GET",
       headers: {
         authorization: `Bearer ${localStorage.getItem("toy-master-token")}`,
@@ -37,12 +37,53 @@ const MyToys = () => {
       });
   }, [user.email]);
 
+  /* -------------------------------------------------------------------------
+    !----------------------- | REMOVE A TOY | -------------------------------
+  ----------------------------------------------------------------------------- */
+  const handelRemoveToy = (id) => {
+    Swal.fire({
+      title: "Want to remove?",
+      icon: "warning",
+      showConfirmButton: true,
+      confirmButtonColor: "red",
+      confirmButtonText: "YES",
+      showCancelButton: true,
+      cancelButtonColor: "green",
+      cancelButtonText: "NO",
+    }).then((swalResult) => {
+      if (swalResult.isConfirmed) {
+        fetch(`http://localhost:5000/remove-toy/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Success!",
+                icon: "success",
+                text: "Remove Successfully",
+              });
+              const remainingToys = myToys.filter((toy) => toy._id !== id);
+              setMyToys(remainingToys);
+            } else {
+              Swal.fire({
+                title: "Error!",
+                icon: "error",
+                text: "Remove Field",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <section className="container mx-auto">
       {loading ? (
         <Loader></Loader>
       ) : (
         <div className="w-3/4 mx-auto text-center text-lg my-10 space-y-5 border rounded-lg py-5">
+          {/* -------------------- | USER INFORMATION | --------------------- */}
           <div className="flex justify-end mr-10">
             <div className="flex items-center gap-5">
               <div className="avatar">
@@ -62,6 +103,7 @@ const MyToys = () => {
             </div>
           </div>
 
+          {/* --------------------------- | MY TOY INFORMATION |------------------- */}
           <div>
             <div className="divider w-3/4 mx-auto my-10"></div>
             {myToys.length === 0 ? (
@@ -73,7 +115,11 @@ const MyToys = () => {
                 <table className="table w-full">
                   <tbody>
                     {myToys.map((toy) => (
-                      <ToyItems key={toy._id} toyInfo={toy}></ToyItems>
+                      <ToyItems
+                        key={toy._id}
+                        toyInfo={toy}
+                        handelRemoveToy={handelRemoveToy}
+                      ></ToyItems>
                     ))}
                   </tbody>
                 </table>
--- src/pages/MyToys/ToyItems.jsx
+++ src/pages/MyToys/ToyItems.jsx
@@ -3,12 +3,12 @@ import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 import { FaInfoCircle, FaRegEdit, FaTrashAlt } from "react-icons/fa";
 
-const ToyItems = ({ toyInfo }) => {
+const ToyItems = ({ toyInfo, handelRemoveToy }) => {
   const { _id, name, category, img, rating, price, quantity } = toyInfo;
   return (
     <tr className="flex justify-between">
-      {/* ----------------------- image and information---------------- */}
-      <td className="w-full flex gap-5 items-center  font-style-script">
+      {/* ----------------------- IMAGE AND INFORMATION ---------------- */}
+      <td className="w-full flex gap-5 items-center ">
         <div className="avatar">
           <div className=" w-40 h-40 rounded-md">
             <img src={img} alt="Avatar Tailwind CSS Component" />
@@ -20,17 +20,22 @@ const ToyItems = ({ toyInfo }) => {
           <div>
             <p>Price: ${price}</p>
             <p>Quantity: {quantity} pc</p>
-            <p className="flex gap-2 items-center tooltip w-fit">
+            <div className="flex gap-2 items-center tooltip w-fit">
               Rating:
               <span className="tooltip tooltip-bottom" data-tip={rating}>
                 <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
               </span>
-            </p>
+            </div>
           </div>
         </div>
       </td>
+
+      {/* ------------------- | Action Button Here | -------------------- */}
       <td className="w-fit gap-5 flex flex-col">
-        <button className="btn text-red-600 btn-outline btn-circle text-xl hover:border-0 hover:bg-red-600 ">
+        <button
+          className="btn text-red-600 btn-outline btn-circle text-xl hover:border-0 hover:bg-red-600 "
+          onClick={() => handelRemoveToy(_id)}
+        >
           <FaTrashAlt></FaTrashAlt>
         </button>
         <button className="btn  text-green-600 btn-outline btn-circle text-xl hover:border-0 hover:bg-green-600">
